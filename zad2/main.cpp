d7cd667dc962de29b8529ebb718ccc7e1effbd4c
Fylyp1010 <77103796+Fylyp19@users.noreply.github.com>
2022-10-10 20:06:24 +0200

    Prototyp Strutkury do Nonogram
    Bardzo podstawowa forma struktury oparta na prostym założeniu, dodaktowo w miarę czytelna wizualizacja całości

--- zad2/main.cpp
+++ zad2/main.cpp
@@ -2,14 +2,62 @@
 #include <vector>
 
 struct puzzle_t{
-    int width;
-    int height;
-    std::vector<int> board;
+    int size; //Musi być jeden wspólny rozmiar
+    std::vector<int> board; //Board służy jako pseudo tablica dla dwóch ciągów
 };
 
 using solution_t = puzzle_t;
 
 std::ostream &operator<<(std::ostream &o, const solution_t &puzzle){
+    //int vertical_line[puzzle.size]; Na przyszłość
+    //int horizontal_line[puzzle.size];
+    using namespace std;
+
+    o << "#";
+
+    for(int i = 0; i < puzzle.size; i++){
+        o << "\t" << puzzle.board[i];
+    }
+    o << std::endl;
+
+
+    for (int y = 0; y < puzzle.size; y++){
+        o << puzzle.board[y+puzzle.size];
+        for (int x = 0; x < puzzle.size ; x++){
+            //Placeholder wydrukowo-wizualizacyjny, wynik nie jest poprawny
+            auto &val = puzzle.board[y*puzzle.size+x];
+            o << "\t" <<((val>0));
+        }
+        o << std::endl;
+    }
+    return o;
+}
+
+int main() {
+    puzzle_t nono{
+        5,
+        //Na wizualizacji lepiej widać jakie znaczenie ma tu board
+        {5, 4, 3, 2, 1,
+               5, 4, 3 ,2, 1 }
+
+    };
+    std::cout << nono;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    puzzle_t nono_bigger{
+            10,
+            //Na wizualizacji lepiej widać jakie znaczenie ma tu board
+            {10,9,8,7,6,5,4,3,2,1,
+             10,9,8,7,6,5,4,3,2,1}
+
+    };
+    std::cout << nono_bigger;
+    return 0;
+}
+
+//Brudnopis
+
+/*std::ostream &operator<<(std::ostream &o, const solution_t &puzzle){
     using namespace std;
     for (int y = 0; y < puzzle.height; y++){
         for (int x = 0; x < puzzle.width; x++){
@@ -19,20 +67,4 @@ std::ostream &operator<<(std::ostream &o, const solution_t &puzzle){
         o << std::endl;
     }
     return o;
-}
-
-int main() {
-    puzzle_t puzzle{
-        5,
-        5,
-        {
-            0,1,0,1,0,
-            1,1,1,1,1,
-            1,1,1,1,1,
-            0,1,1,1,0,
-            0,0,1,0,0
-        }
-    };
-    std::cout << puzzle;
-    return 0;
-}
+}*/
